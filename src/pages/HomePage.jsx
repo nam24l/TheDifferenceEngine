4b67c203e60d2c546aa96eb54737c9e63156282c
Nelson <nam24l@fsu.edu>
2025-02-20 17:47:28 -0500

    Cleaned up more.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ import ViewBlogPosts from '../components/ViewBlogPosts'
 const HomePage = () => {
   return (
     <>
-    <Hero title={"Hi! I am Nelson Munoz-Traxler"} subtitle={"Florida State Class of '28 | Political Science Major | Computer Science Minor | Eco-Thinker | Developer | Entrepreneur | Lifelong Learner | Innovator"}/>
+    <Hero title={"Hi! I am Nelson Munoz-Traxler"} subtitle={"Florida State Class of '28 | Political Science Major | Computer Science Minor | Entrepreneur | Lifelong Learner | Innovator"}/>
     <HomeCards/>
     <PostListings/>
     <ViewBlogPosts/>
