0d350d088572563b5ae441e84fb78886c8afc0a8
Nelson <nam24l@fsu.edu>
2025-02-23 22:24:29 -0500

    Updated bio

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ import ViewBlogPosts from '../components/ViewBlogPosts'
 const HomePage = () => {
   return (
     <>
-    <Hero title={"Hi! I am Nelson Munoz-Traxler"} subtitle={"Florida State Class of '28 | Political Science Major | Computer Science Minor | Entrepreneur | Lifelong Learner | Innovator"}/>
+    <Hero title={"Hi! I am Nelson Munoz-Traxler"} subtitle={"Florida State Class of '28 | Political Science Major | Information Technology Minor | Entrepreneur | Lifelong Learner | Innovator"}/>
     <HomeCards/>
     <PostListings/>
     <ViewBlogPosts/>
